862884a5effa858ae4d7cb15df3f776518437776
jacopodisi <disimone.jacopo@mail.polimi.it>
2017-03-24 00:12:51 +0100

    [functions] added documentation

--- include/functions.h
+++ include/functions.h
@@ -9,6 +9,13 @@ namespace Functions {
 	check if a file exists
 	*/
 	bool fileExists(const std::string& filename);
+
+	/*
+	save the matrix rows+cols+data with rows and cols as uint, data as double
+	in a .bin file specified.
+	@param the matrix to save, the name of the file, the direcotry to which
+	       save the file
+	*/
 	void save(std::shared_ptr<Eigen::MatrixXd> mat, std::string filename, std::string directoryname);
 
 } // Functions
