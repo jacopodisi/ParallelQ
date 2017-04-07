6c84097ed3f3af3730740ce86250951158c99bb2
jacopodisi <disimone.jacopo@mail.polimi.it>
2017-04-07 11:27:31 +0200

    some stuff

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
-for ((np=1; np <= 8; np=np*2))
-do
-    for ((id=0; id <= 4; id++))
+for (( i = 0; i < 30; i++ )); do
+	for ((np=1; np <= 8; np=np*2))
 	do
-	    ./myprog.out $id $np --save_stat
+	    for ((id=0; id <= 10; id++))
+		do
+		    ./myprog.out $id $np --save_time
+		done
 	done
 done
